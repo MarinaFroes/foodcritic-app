09c12271d78e8ee472607d7f83f436b9d20f041b
MFAC <ninafroes@gmail.com>
2019-10-01 17:33:14 +0200

    feat: Import Author and AUTHOR_INFO

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,53 +1,33 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 import styled from 'styled-components';
+import Author from './Author';
+import { AUTHOR_INFO } from './authorsInfo';
 
 const FooterContainer = styled.footer`
   display: flex;
   justify-content: center;
-  flex-direction: column;
+  flex-wrap: wrap;
   text-align: center;
-  width: 100%;
   color: #000;
-  padding: 2rem;
+  margin: 1rem;
+  border-top: 1.5px solid #f2f2f2;
 `;
-
-const IconContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Icon = styled.a`
-  color: silver;
-  font-size: 1.5rem;
-  margin: 0 1rem;
-`;
-
-const MARINA_LINKEDIN = "https://www.linkedin.com/in/marina-froes-a-costa/";
-const MARINA_GITHUB = "https://github.com/MarinaFroes";
-const MARINA_GMAIL = "facosta.marina@gmail.com";
 
 export default function Footer() {
   return (
     <FooterContainer id="contact">
-      <p>Front-End by Marina Costa</p>
-      <IconContainer>
-        <Icon href={MARINA_GITHUB} target="_blank">
-          {<FaGithub />}
-        </Icon>
-        
-        <Icon
-          href={MARINA_LINKEDIN}
-          target="_blank"
-        >
-          {<FaLinkedin />}
-        </Icon>
-
-        <Icon href={`mailto:${MARINA_GMAIL}`} target="_blank">
-          {<FaEnvelope />}
-        </Icon>
-      </IconContainer>
+      {
+        AUTHOR_INFO.map(info => (
+          <Author
+            key={info.name}
+            name={info.name}
+            stack={info.stack}
+            github={info.github}
+            linkedin={info.linkedin}
+            gmail={info.gmail}
+          />
+        ))
+      }
     </FooterContainer>
   )
 };
