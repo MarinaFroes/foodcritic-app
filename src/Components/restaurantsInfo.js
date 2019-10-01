5199b5972b3bc0d19d6bf72d5f6c1da34a84523f
MFAC <ninafroes@gmail.com>
2019-10-01 17:34:01 +0200

    feat: Add RESTAURANT_INFO file

---
+++ src/Components/restaurantsInfo.js
@@ -0,0 +1,20 @@
+export const RESTAURANT_INFO = [
+  {
+    name: "Restaurant 1",
+    category: "Category 1",
+    location: "Location 1",
+    rate: 10
+  },
+  {
+    name: "Restaurant 2",
+    category: "Category 2",
+    location: "Location 2",
+    rate: 9
+  },
+  {
+    name: "Restaurant 3",
+    category: "Category 3",
+    location: "Location 3",
+    rate: 5
+  }
+]
