21638740056939a98cff2f6daa4965bbb43f4adf
MFAC <ninafroes@gmail.com>
2019-10-01 17:33:27 +0200

    styles: Adjust padding and margin

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -5,12 +5,13 @@ const RestaurantInfo = styled.li`
   display: flex;
   flex-direction: column;
   background-color: #f2f2f2;
-  margin: 0.5rem 1rem;
+  margin: 1rem 1rem;
   padding: 1rem;
 `;
 
 const Name = styled.h3`
   font-size: 1rem;
+  margin-bottom: 0.5rem;
 `;
 
 const Info = styled.p`
