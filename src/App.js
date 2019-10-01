5e0b70f029258d0fcf17ea5767c3dc367b0a68af
MFAC <ninafroes@gmail.com>
2019-10-01 15:59:43 +0200

    feat: Import Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from '../src/Components/Footer';
+import Header from '../src/Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Avalio</h1>
+      <Header title="Avalio"/>
+      <Footer />
     </div>
   );
 }
