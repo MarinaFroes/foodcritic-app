e86128813c1c5f15a85cd448e973a87422e7e777
MFAC <ninafroes@gmail.com>
2019-10-01 17:32:46 +0200

    feat: Add authorsInfo

---
+++ src/Components/authorsInfo.js
@@ -0,0 +1,16 @@
+export const AUTHOR_INFO = [
+  {
+    stack: "Front-End",
+    name: "Marina Costa",
+    linkedin: "https://www.linkedin.com/in/marina-froes-a-costa/",
+    github: "https://github.com/MarinaFroes",
+    gmail: "facosta.marina@gmail.com"
+  },
+  {
+    stack: "Back-End",
+    name: "Magno Ferreira Jr",
+    linkedin: "https://www.linkedin.com/in/magno-junior-78a755111/",
+    github: "https://github.com/JMagnoJunior",
+    gmail: "is.magnojr@gmail.com"
+  }
+]
