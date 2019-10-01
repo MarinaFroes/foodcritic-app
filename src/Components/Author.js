d075777bf04701dde5a81e753b1bf2c8415f88fd
MFAC <ninafroes@gmail.com>
2019-10-01 17:32:18 +0200

    feat: Add Author component

---
+++ src/Components/Author.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const AuthorInfo = styled.div`
+  margin: 0.5rem;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Icon = styled.a`
+  color: silver;
+  font-size: 1rem;
+  margin: 0.3rem 1rem;
+`;
+
+export default function Author({ name, github, linkedin, gmail, stack }) {
+  return (
+    <React.Fragment>
+      <AuthorInfo>
+        <p>{stack}: {name}</p>
+        <IconContainer>
+          <Icon href={github} target="_blank">
+            {<FaGithub />}
+          </Icon>
+          <Icon
+            href={linkedin}
+            target="_blank"
+          >
+            {<FaLinkedin />}
+          </Icon>
+          <Icon href={`mailto:${gmail}`} target="_blank">
+            {<FaEnvelope />}
+          </Icon>
+        </IconContainer>
+      </AuthorInfo>
+    </React.Fragment>
+  )
+}
