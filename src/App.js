f791bcffac783910a9f81692023e62cee63c8af0
MFAC <ninafroes@gmail.com>
2019-10-01 17:04:02 +0200

    feat: Import RestaurantList

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Footer from '../src/Components/Footer';
 import Header from '../src/Components/Header';
+import RestaurantList from '../src/Components/RestaurantList';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Avalio"/>
+      <Header title="Avalio" />
+      <RestaurantList />
       <Footer />
     </div>
   );
