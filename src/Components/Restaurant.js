83402c19877e195553ef058c7410744892a083f0
MFAC <ninafroes@gmail.com>
2019-10-08 17:46:23 +0200

    feat: Change styles and info

--- src/Components/Restaurant.js
+++ src/Components/Restaurant.js
@@ -5,26 +5,28 @@ const RestaurantInfo = styled.li`
   display: flex;
   flex-direction: column;
   background-color: #f2f2f2;
-  margin: 1rem 1rem;
+  margin: 0.5rem 0;
   padding: 1rem;
+  height: 6rem;
 `;
 
 const Name = styled.h3`
   font-size: 1rem;
-  margin-bottom: 0.5rem;
+  margin: 0.5rem;
 `;
 
-const Info = styled.p`
-  font-size: 0.8rem;
-`;
+// const Info = styled.p`
+//   font-size: 0.8rem;
+// `;
 
 export default function Restaurant({ name, category, location, rate }) {
   return (
     <RestaurantInfo>
-      <Name>{name}</Name>
-      <Info>Category: {category}</Info>
+      <Name>{name} - {location}</Name>
+      <p>Rate: {rate}</p>
+      {/* <Info>Category: {category}</Info>
       <Info>Location: {location}</Info>
-      <Info>Rating: {rate}</Info>
+      <Info>Rating: {rate}</Info> */}
     </RestaurantInfo>
   )
 }
