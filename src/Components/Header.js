bc1045e0f242d1b81944d2f4c29c796e875b07d2
MFAC <ninafroes@gmail.com>
2019-10-08 17:44:50 +0200

    feat: Add logo and change styles

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import logo from "./images/logo.svg";
+
 
 const HeaderContainer = styled.header`
   display: flex;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
-  text-align: center;
-  background-color: #fff;
-  color: #000;
+  background-image: linear-gradient(#193377, #8C46BD);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   width: 100%;
+  height: 5rem;
+  padding: 0 1rem;
 `;
 
 const Heading = styled.h1`
   font-size: 2rem;
-  padding: 1.5rem;
+  color: #fff;
+  margin-left: 1rem;
+`;
+
+const Logo = styled.img`
+  height: 2rem;
 `;
 
 export default function Header( { title }) {
   return (
     <HeaderContainer id="header">
+      <Logo src={logo} alt=""/>
       <Heading>{title}</Heading>
     </HeaderContainer>
   )
