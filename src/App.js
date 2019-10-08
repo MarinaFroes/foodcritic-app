630b259b211ecefd850c2b256c67c46093906c37
MFAC <ninafroes@gmail.com>
2019-10-08 17:44:29 +0200

    feat: Change app title to FoodCritic and comment Footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import Footer from '../src/Components/Footer';
+// import Footer from '../src/Components/Footer';
 import Header from '../src/Components/Header';
 import RestaurantList from '../src/Components/RestaurantList';
 
 function App() {
   return (
     <div className="App">
-      <Header title="Avalio" />
+      <Header title="FoodCritic" />
       <RestaurantList />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
