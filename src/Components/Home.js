84e67e93285d704ceec0c9f0563c966e07418b80
MFAC <ninafroes@gmail.com>
2019-10-09 18:25:49 +0200

    feat: Create Home page

---
+++ src/Components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import BgImg from './images/joseph-gonzalez-unsplash.jpg';
+
+const MainPage = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  height: 90vh;
+  background: url(${BgImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #c4c4c4;
+  width: 200px;
+  height: 40px;
+  margin-bottom: 80px;
+  border-radius: 5px;
+  font-size: 20px;
+  color: #000;
+
+  &:hover {
+    background-color: #7e7e7e;
+    cursor: pointer;
+  }
+`;
+
+export default function Home() {
+  return (
+    <MainPage>
+      <Link to="/restaurant_list" style={{textDecoration: 'none'}}>
+        <LinkContainer>View List</LinkContainer>
+      </Link>
+    </MainPage>
+  )
+}
