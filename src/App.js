8940788617cb882dc178ee9257e6579471744567
MFAC <ninafroes@gmail.com>
2019-10-09 18:26:22 +0200

    feat: Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 // import Footer from '../src/Components/Footer';
+import Home from './Components/Home';
 import Header from '../src/Components/Header';
 import RestaurantList from '../src/Components/RestaurantList';
 
 function App() {
   return (
     <div className="App">
-      <Header title="FoodCritic" />
-      <RestaurantList />
-      {/* <Footer /> */}
+      <BrowserRouter>
+        <Header title="FoodCritic" />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/restaurant_list" component={RestaurantList } />
+        {/* <Footer /> */}
+      </BrowserRouter>
     </div>
   );
 }
