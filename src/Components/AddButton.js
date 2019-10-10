94fc13dce12a516a57ac6e8ae865e242f7f96169
MFAC <ninafroes@gmail.com>
2019-10-10 15:43:39 +0200

    feat: Add link to RestaurantEditing page

--- src/Components/AddButton.js
+++ src/Components/AddButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import { Link } from 'react-router-dom';
 
 export default function AddButton() {
   return (
     <React.Fragment>
-      <Fab size="medium" aria-label="add">
-        <AddIcon />
-      </Fab>
+      <Link to="/restaurant_editing" style={{ textDecoration: 'none' }}>
+        <Fab size="medium" aria-label="add">
+          <AddIcon />
+        </Fab>
+      </Link>
     </React.Fragment>
   )
 }
