5b9df7cf8e461537266765e703bbb792c7cc0be3
MFAC <ninafroes@gmail.com>
2019-10-17 21:08:22 +0200

    feat: Add Route to Login component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 // import Footer from '../src/Components/Footer';
+import Login from './Components/Login';
 import Home from './Components/Home';
 import Header from '../src/Components/Header';
 import RestaurantList from '../src/Components/RestaurantList';
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Header title="FoodCritic" />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Home} />
         <Route exact path="/restaurant_list" component={RestaurantList} />
         <Route exact path="/restaurant_editing" component={RestaurantEditing} />
