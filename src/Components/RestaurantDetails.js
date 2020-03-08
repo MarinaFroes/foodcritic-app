16939d1214146fa39aa84af0d9ae8a920252b477
MFAC <ninafroes@gmail.com>
2020-03-08 14:24:33 +0100

    styles: Add styles to RestaurantDetails

--- src/Components/RestaurantDetails.js
+++ src/Components/RestaurantDetails.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 
-function RestaurantDetails({ name, location, rid, category }) {
+const RestaurantContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const StyledTitle = styled.h1`
+  font-size: 1.5rem;
+  padding: 1rem;
+`
+const StyledInfo = styled.p`
+  font-size: 1.2rem;
+  padding: 0.5rem;
+`
+
+function RestaurantDetails({ name, location, rid, category, cuisine }) {
   return (
-    <div>
-      <p>Name: {name}</p>
-      <p>Location: {location}</p>
-      <p>rid: {rid}</p>
-      <p>Category: {category}</p>
-    </div>
+    <RestaurantContainer>
+      <StyledTitle>{name}</StyledTitle>
+      <StyledInfo>Location: {location}</StyledInfo>
+      <StyledInfo>rid: {rid}</StyledInfo>
+      <StyledInfo>Category: {category}</StyledInfo>
+      <StyledInfo>Cuisine: {cuisine}</StyledInfo>
+    </RestaurantContainer>
   )
 }
 
