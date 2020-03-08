ba5238120070f8c0e4871aae46f510d89f87fc36
MFAC <ninafroes@gmail.com>
2020-03-08 10:15:05 +0100

    feat: Add Route to RestaurantPage and remove semi colon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import { HashRouter as Router , Route } from 'react-router-dom';
-import './App.css';
-// import Footer from '../src/Components/Footer';
-import Login from './Components/Login';
-import Home from './Components/Home';
-import Header from '../src/Components/Header';
-import RestaurantList from '../src/Components/RestaurantList';
-import RestaurantEditing from '../src/Components/RestaurantEditing';
+import React from 'react'
+import { HashRouter as Router , Route } from 'react-router-dom'
+import './App.css'
+// import Footer from '../src/Components/Footer'
+import Login from './Components/Login'
+import Home from './Components/Home'
+import Header from '../src/Components/Header'
+import RestaurantList from '../src/Components/RestaurantList'
+import RestaurantEditing from '../src/Components/RestaurantEditing'
+import RestaurantPage from '../src/Components/RestaurantPage'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/restaurant_list" component={RestaurantList} />
         <Route exact path="/restaurant_editing" component={RestaurantEditing} />
+        <Route path="/restaurant/:rid" component={RestaurantPage} />
         {/* <Footer /> */}
       </Router>
     </div>
