8676f1c667f214fc1a75d50721be42f8819fb064
MFAC <ninafroes@gmail.com>
2020-03-10 17:43:47 +0100

    refactor: Remove semi-colons

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import styled from 'styled-components';
-import Author from './Author';
-import { AUTHOR_INFO } from './authorsInfo';
+import React from 'react'
+import styled from 'styled-components'
+
+import Author from './Author'
+import { AUTHOR_INFO } from './authorsInfo'
 
 const FooterContainer = styled.footer`
   display: flex;
