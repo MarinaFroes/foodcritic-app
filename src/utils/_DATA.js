247c5672ad15b4880bcf2a4614be1e3684e0c27a
MFAC <ninafroes@gmail.com>
2020-03-10 18:38:41 +0100

    feat: Remove users methods and author info and add getRestaurant method

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,69 +2,28 @@ let restaurants = [
   {
     rid: "am8ehyc8byjqgar0jgpub9",
     name: "Ishin",
-    author: "sarahedo",
     category: "restaurant",
     cuisine: "japanese",
     location: "Wilmersdorf",
-    ratings: {
-      "sarahedo": 4,
-      "marinacosta": 5
-    },
-    raters: ["sarahedo", "marinacosta"]
+    rating: 4
   },
   {
     rid: "6ni6ok3ym7mf1p33lnez",
     name: "Monsieur Vuong",
-    author: "sarahedo",
     category: "restaurant",
     cuisine: "vietnamese",
     location: "Mitte",
-    ratings: {
-      "sarahedo": 3,
-      "marinacosta": 4
-    },
-    raters: ["sarahedo", "marinacosta"]
+    rating: 3
   },
   {
     rid: "8xf0y6ziyjabvozdd253nd",
     name: "Shanius",
-    author: "sarahedo",
     category: "restaurant",
     cuisine: "chinese",
     location: "Wilmersdorf",
-    ratings: {
-      "sarahedo": 5,
-      "marinacosta": 3
-    },
-    raters: ["sarahedo", "marinacosta"]
+    rating: 3
   }
 ]
-
-let users = [
-  {
-    uid: "sarahedo",
-    name: "Sarah Edo",
-    avatarURL: "https://picsum.photos/id/1027/200/200",
-    ratings: {
-      "am8ehyc8byjqgar0jgpub9": 4,
-      "6ni6ok3ym7mf1p33lnez": 3,
-      "8xf0y6ziyjabvozdd253nd": 5
-    },
-    restaurants: ["am8ehyc8byjqgar0jgpub9", "6ni6ok3ym7mf1p33lnez", "8xf0y6ziyjabvozdd253nd"]
-  },
-  {
-    uid: "marinacosta",
-    name: "Marina Costa",
-    avatarURL: "https://picsum.photos/id/1027/200/200",
-    ratings: {
-      "am8ehyc8byjqgar0jgpub9": 5,
-      "6ni6ok3ym7mf1p33lnez": 4,
-      "8xf0y6ziyjabvozdd253nd": 3
-    },
-    restaurants: ["am8ehyc8byjqgar0jgpub9", "6ni6ok3ym7mf1p33lnez", "8xf0y6ziyjabvozdd253nd"]
-  }
-]
-
 
 function generateID () {
   return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
@@ -76,24 +35,24 @@ export function _getRestaurants () {
   })
 }
 
-export function _getUsers () {
+export function _getRestaurant (rid) {
   return new Promise((res, rej) => {
-    setTimeout(() => res([...users]), 1000)
+    setTimeout(() => {
+      const restaurant = restaurants.filter(restaurant => restaurant.rid === rid)
+
+      res(restaurant)
+    }, 1000)
   })
 }
 
-function formatRestaurant({ name, category, cuisine, location, author, rating }, rid ) {
+function formatRestaurant({ name, category, cuisine, location, rating }, rid ) {
   return {
     rid,
     name,
-    author,
     category,
     cuisine,
     location,
-    ratings: {
-      [author]: rating
-    },
-    raters: [author]
+    rating
   }
 }
 
@@ -107,81 +66,25 @@ export function _saveRestaurant (restaurant) {
         ...restaurants,
         formattedRestaurant
       ]
-      
-      users = users.map((user) => {
-        if (user.uid === formattedRestaurant.author) {
-          user.ratings[rid] = Number(restaurant.rating)
-          user.restaurants.push(rid)
-          return user
-        } else {
-          return user
-        }
-      })
-
+    
       res(formattedRestaurant)
     }, 1000)
   })
 }
 
-export function _saveRating({ authedUser, rid, rating }) {
+export function _editRestaurant(newRestaurant, rid ) {
   return new Promise((res, rej) => {
     setTimeout(() => {
-      // users = {
-      //   ...users,
-      //   [authedUser]: {
-      //     ...users[authedUser],
-      //     ratings: users[authedUser].ratings[rid] = rating,
-      //     restaurants: users[authedUser].restaurants.concat([rid])
-      //   }
-      // }
+      const formattedRestaurant = formatRestaurant(newRestaurant, rid)
+      
+      const existingRestaurants = restaurants.filter(restaurant => restaurant.rid !== rid)
 
-      // restaurants = {
-      //   ...restaurants,
-      //   [rid]: {
-      //     ...restaurants[rid],
-      //     ratings: restaurants[rid].ratings[authedUser] = rating,
-      //     raters: restaurants[rid].raters.concat([authedUser])
-      //   }
-      // }
+      restaurants = [
+        ...existingRestaurants,
+        formattedRestaurant
+      ]
 
-      res()
-    }, 500)
-  })
-}
-// export function _saveQuestionAnswer ({ authedUser, qid, answer }) {
-//   return new Promise((res, rej) => {
-//     setTimeout(() => {
-//       users = {
-//         ...users,
-//         [authedUser]: {
-//           ...users[authedUser],
-//           answers: {
-//             ...users[authedUser].answers,
-//             [qid]: answer
-//           }
-//         }
-//       }
-
-//       questions = {
-//         ...questions,
-//         [qid]: {
-//           ...questions[qid],
-//           [answer]: {
-//             ...questions[qid][answer],
-//             votes: questions[qid][answer].votes.concat([authedUser])
-//           }
-//         }
-//       }
-
-//       res()
-//     }, 500)
-//   })
-// }
-
-export function _editRestaurant(restaurant) {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-       console.log('editing restaurant')
+      res(restaurants)
     }, 1000)
   })
 }
